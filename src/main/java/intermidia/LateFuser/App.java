ee33558280c4c0cee920013ec1775d4b6d7e421e
rodrigokishi <rodrigokishi@usp.br>
2017-08-26 17:52:47 -0300

    Initial upload

---
+++ src/main/java/intermidia/LateFuser/App.java
@@ -0,0 +1,13 @@
+package intermidia.LateFuser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
